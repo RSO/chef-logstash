f4bab5b2bb2b115507b0455362195abc717ffb48
Remon Oldenbeuving <r.s.oldenbeuving@gmail.com>
2013-04-22 13:40:00 +0200

    Moved tcp-input to default, because, let's face it, that's where they are for, right?

--- attributes/server.rb
+++ attributes/server.rb
@@ -4,7 +4,13 @@ default['logstash']['server']['base_config_cookbook'] = 'logstash'
 # roles/flags for various autoconfig/discovery components
 default['logstash']['server']['enable_embedded_es'] = true
 
-default['logstash']['server']['inputs'] = []
+default['logstash']['server']['inputs'] = [{
+  :tcp => {
+    :type => 'tcp-input',
+    :port => '5959',
+    :format => 'json_event'
+  }
+}]
 default['logstash']['server']['filters'] = []
 default['logstash']['server']['outputs'] = []
 
